797b4bce581510311f0668b1f4e5d3b05409d545
Wishwas <wishwasmeghawal1441@gmail.com>
2026-03-04 23:01:10 +0530

    Feature: Basic Express app with mongoose setup done

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const env = require('dotenv');
+const mongoose  = require('mongoose');
+
+env.config();
+const app = express(); // express app object
+const port = process.env.PORT;
+
+app.use(bodyParser.urlencoded({extends: true}))
+app.use(bodyParser.json())
+
+app.get('/home',(req, res)=>{
+    return res.json({
+        success:true,
+        error:false,
+        message:"this is the home page"
+    })
+    
+})
+app.listen(port, async ()=>{
+    // this callback gets executed , once we successfully start the server on the given port
+
+    console.log(`Server Started on Port ${port}`);
+
+
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        console.log("Successfully connected to Database");
+    } catch (error) {
+        console.log("Not able to connect Database",error);
+        
+    }
+   
+
+});
