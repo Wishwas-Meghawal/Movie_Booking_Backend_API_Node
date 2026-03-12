294b63178baa4711c67c3734f53eab09d7d0b35c
Wishwas <wishwasmeghawal1441@gmail.com>
2026-03-12 22:49:16 +0530

    Feat: Addes Delete Movie API

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -5,6 +5,8 @@ const routes = (app) =>{
   // routes function takes express app object as parameter
 
   app.post('/mba/api/v1/movies',MovieController.createMovie);
+
+  app.delete('/mba/api/v1/movies/:movieId',MovieController.deleteMovie)
 }
 
 module.exports = routes;
