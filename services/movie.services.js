70bc856fdb74f7ec91bf9eeca493d0511140ffb3
Wishwas <wishwasmeghawal1441@gmail.com>
2026-03-14 23:32:30 +0530

    feat: add movie service to fetch movie by id

---
+++ services/movie.services.js
@@ -0,0 +1,17 @@
+const Movie = require('../models/movie.model.js');
+
+const getMovieById  = async (id) =>{
+  const movie = Movie.findById(id);
+
+  if(!movie){
+    return{
+      err: "No movie found for the corresponding id provided",
+      code: 404,
+    }
+  };
+  return movie;
+}
+
+module.exports = {
+  getMovieById
+}
